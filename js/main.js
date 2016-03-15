e3c1fbecd82a30145bfa74d9db53b0b1ebec373b
brenmoods <brendanm@vla.vic.gov.au>
2016-03-15 17:49:22 +1100

    update

--- js/main.js
+++ js/main.js
@@ -15,20 +15,57 @@ $(document).ready(function () {
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
         }, 900, 'swing');
-
-
     });
 
-
-
-
-//on click id victoria
+    //on click id victoria
     $("#victoria").click(function ($e) {
-
         //prevents default action of element - page scrolling back to top of page
         $e.preventDefault();
-
-        //display hidden id vicInfo with a fade toggle
+        //display / hide hidden id vicInfo with a fade toggle
         $("#vicArea").fadeToggle();
     });
+
+    //on click id albion
+    $("#albionClick").click(function ($e) {
+        //prevents default action of element - page scrolling back to top of page
+        $e.preventDefault();
+        //display / hide hidden id vicInfo with a fade toggle
+        $("#albion").fadeToggle();
+    });
+
+    $("#eppingClick").click(function ($e) {
+        $e.preventDefault();
+        $("#epping").fadeToggle();
+    });
+
+    $("#hcrossClick").click(function ($e) {
+        $e.preventDefault();
+        $("#hcross").fadeToggle();
+    });
+
+    $("#kealbaClick").click(function ($e) {
+        $e.preventDefault();
+        $("#kealba").fadeToggle();
+    });
+
+    $("#lalorClick").click(function ($e) {
+        $e.preventDefault();
+        $("#lalor").fadeToggle();
+    });
+
+    $("#prestonClick").click(function ($e) {
+        $e.preventDefault();
+        $("#preston").fadeToggle();
+    });
+
+    $("#stAlbansClick").click(function ($e) {
+        $e.preventDefault();
+        $("#stAlbans").fadeToggle();
+    });
+    
+    $("#sunWestClick").click(function ($e) {
+        $e.preventDefault();
+        $("#sunWest").fadeToggle();
+    });
+
 });
